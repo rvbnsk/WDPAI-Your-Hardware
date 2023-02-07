11287e1b4686fb0b1c45cd9e4225bdc64b892a34
rvbnsk <58656716+rvbnsk@users.noreply.github.com>
2023-02-07 14:17:14 +0100

    renamed main to home

--- src/html/home.php
+++ src/html/home.php
@@ -6,13 +6,14 @@
   <meta name="description" content=""/>
   <meta name="keywords" content=""/>
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="../css/main.css" type="text/css">
+  <link rel="stylesheet" href="src/css/main.css" type="text/css">
   <title>Your Hardware</title>
   <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400&display=swap" rel="stylesheet">
 </head>
 
-<body class="main_page">
 
+
+<body class="main_page">
   <div class="menu-wrap">
     <input type="checkbox" class="toggler">
     <div class="hamburger"><div></div></div>
@@ -20,13 +21,20 @@
       <div>
         <div>
           <ul>
-            <li><a href="main.html">Main Page</a></li>
-            <li><a href="posts.html">Posts</a></li>
-            <li><a href="add.html">Add Post</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="contact.html">Contact</a></li>
-            <li><a href="register.html">Register</a></li>
-            <li><a href="login.html">Login</a></li>
+            <li><a href="home">Main Page</a></li>
+            <li><a href="posts">Posts</a></li>
+            <li><a href="addPost">Add Post</a></li>
+            <li><a href="about">About</a></li>
+            <li><a href="contact">Contact</a></li>
+            <?php
+            session_start();
+            if(!$_SESSION["isLoggedIn"]) {
+              echo '<li><a href="login">Login</a></li>';
+              echo '<li><a href="register">Register</a></li>';
+            } else {
+              echo '<li><a href="logout">Logout</a></li>';
+            }
+            ?>
           </ul>
         </div>
       </div>
