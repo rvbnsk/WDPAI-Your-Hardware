8526cac50ba33fdb4870449d49b630aa1e6fc199
rvbnsk <58656716+rvbnsk@users.noreply.github.com>
2023-02-07 12:06:34 +0100

    added posts details

---
+++ src/models/js/posts_details.js
@@ -0,0 +1,31 @@
+const fetchDetails = async () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const data = await fetch(
+      `http://localhost:8080/post?id=${urlParams.get("id")}`
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((element) => {
+        let parentElement = document.querySelector(".post_title");
+        let postText = document.querySelector(".post_text");
+        let postPicture = document.querySelector(".post_picture");
+  
+        const newElement = document.createElement("h3");
+        newElement.innerHTML = `${element.title}`;
+  
+        const newPostText = document.createElement("h3");
+        newPostText.innerHTML = `${element.description}`;
+  
+        const newImg = document.createElement("div");
+        newImg.innerHTML = `<img src="${element.imageurl}" alt="" width=300 />`;
+  
+        parentElement.appendChild(newElement);
+        postText.appendChild(newPostText);
+        postPicture.appendChild(newImg);
+      });
+  };
+  
+  fetchDetails();
+  
