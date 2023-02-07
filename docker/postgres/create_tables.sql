541a8022590d3f436209638ab74242a5767b7cd8
rvbnsk <58656716+rvbnsk@users.noreply.github.com>
2023-02-07 11:56:17 +0100

    added postgres

---
+++ docker/postgres/create_tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users (
+    id serial not null,
+    name varchar(100) not null,
+    surname varchar(100) not null,
+    email varchar(100) not null unique,
+    password varchar(100) not null
+    -- role_id int not null foreign key()
+);
+
+
+CREATE TABLE data (
+    id serial not null,
+    title varchar(100) not null,
+    description varchar(100) not null,
+    createdBy varchar(100) not null,
+    imageUrl varchar(100) not null
+);
+
+-- CREATE TABLE roles (
+--     id serial not null,
+--     name varchar(100) not null
+-- );
