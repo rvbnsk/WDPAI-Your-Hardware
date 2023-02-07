2b6f4829b8b77aa22a47d58f50d9b6b797bc48e8
rvbnsk <58656716+rvbnsk@users.noreply.github.com>
2023-02-07 13:40:17 +0100

    added index with routing

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
 
-echo "Template";
+require_once 'src/Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('index', 'AppController');
+Routing::get('home', 'AppController');
+
+Routing::post('register', 'LoginController');
+Routing::post('login', 'LoginController');
+
+Routing::get('admin', 'ContentController');
+Routing::post('addPost', 'ContentController');
+Routing::get('getPosts', 'ContentController');
+Routing::get('posts', 'ContentController');
+Routing::get('about', 'ContentController');
+Routing::get('logout', 'LoginController');
+
+Routing::get('post', 'ContentController');
+Routing::get('postDetails', 'ContentController');
+Routing::get('contact', 'ContentController');
+
+if($path == "/") {
+    $path = "index";
+}
+
+Routing::run($path);
